2cc8e43f96701a9ce96db74cedef484bb0126bcd
ʇɐʞıɐs ɐʇʇnp <saikatdutta1991@users.noreply.github.com>
2020-01-10 17:19:44 +0530

    Update fizzBuzz.js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,28 +1,40 @@
-var options  = { 3 : "Fizz", 5 : "Buzz" }
+<script>
 
-    function isDivisibleBy(n, m) {
-        return n % m == 0;
-    }
+    function makeFizzBuzzLogger(n, options) {
 
-    function fizzBuzz(n, options) {
-        console.log(n);
-        let multipliers = Object.keys(options);
-        let result = "";
+        let multipliers = Object.keys(options).map( x => parseInt(x));
 
-        multipliers.forEach((m) => {
-            if(isDivisibleBy(n, m)) {
-                result += options[m];
-            }
-        });
-
-        return result.length ? result : n;
-
-    }
-
-    function fizzBuzzLogger(n, options) {
-
-        for(let i = 1; i <= n; i++) {
-            console.log(fizzBuzz(i, options))
+        
+        function isDivisible(n, m) {
+            return n % m == 0;
         }
 
+        function fizzBuzz(n) {
+        
+            let fizzbuzz =  multipliers.reduce((res, multiplier) => {
+                return res + ( isDivisible(n, multiplier) ? options[multiplier] : "" );
+            }, "");
+
+            return fizzbuzz.length ? fizzbuzz : n; 
+        }
+
+        function run() {
+        
+            let numbers = Array.from({ length : n }, (x,i) => i + 1);
+            return numbers.map( x => fizzBuzz(x));
+        }
+
+        return { run : run };
     }
+
+
+
+
+
+    var options  = { 3 : "Fizz", 5 : "Buzz", 7 : "Woff" };
+    var results = makeFizzBuzzLogger(50, options).run();
+
+    console.log(results);
+
+
+</script>
