5ec486a2da0b00c0d485550e019e5c4556bd1bdc
Saikat Dutta <saikatdutta1991@gmail.com>
2020-03-21 11:38:00 +0530

    eslint applied

--- merge_sort.js
+++ merge_sort.js
@@ -1,61 +1,42 @@
-<script>
-
-
 function mergeSort(arr) {
+  let newArray = arr.map(x => x);
 
-    let newArray = arr.map( x => x );
+  const sort = function(low, high) {
+    if (low < high) {
+      let middle = parseInt((low + high) / 2);
+      sort(low, middle);
+      sort(middle + 1, high);
+      merge(low, middle, high);
+    }
+  };
 
-    const sort = function(low, high) {
+  const merge = function(low, middle, high) {
+    let tempArr = [],
+      i = low,
+      j = middle + 1;
 
-        if( low < high ) {
-
-            let middle = parseInt( ( low + high ) / 2 );
-            sort( low, middle );
-            sort( middle + 1, high );
-            merge( low, middle, high );
-            
-        }
-
-        
-    };
-
-    const merge = function(low, middle, high) {
-
-        let tempArr = [], 
-            i = low, 
-            j = middle + 1;
-
-        while( i <= middle && j <= high ) {
-            tempArr.push( newArray[ i ] < newArray[ j ] ? newArray[ i++ ] : newArray[ j++ ] );
-        }
-
-        while( i <= middle ) {
-            tempArr.push( newArray[ i++ ] );
-        }
-
-        while( j <= high ) {
-            tempArr.push( newArray[ j++ ] );
-        }
-
-        i = low;
-        tempArr.forEach( v => {
-            newArray[ i++ ] = v;
-        });
-
+    while (i <= middle && j <= high) {
+      tempArr.push(newArray[i] < newArray[j] ? newArray[i++] : newArray[j++]);
     }
 
+    while (i <= middle) {
+      tempArr.push(newArray[i++]);
+    }
 
-    sort( 0, newArray.length - 1 );
+    while (j <= high) {
+      tempArr.push(newArray[j++]);
+    }
 
-    return newArray;
+    i = low;
+    tempArr.forEach(v => {
+      newArray[i++] = v;
+    });
+  };
+
+  sort(0, newArray.length - 1);
+
+  return newArray;
 }
 
-
-
-
-
 let arr = [3, 5, 7, 1, 2, 8, 9];
-console.log( mergeSort( arr ) );
-
-
-</script>
+console.log(mergeSort(arr));
