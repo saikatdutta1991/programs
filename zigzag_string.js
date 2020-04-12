3d09d1ded54373031d0a74a31eafb2d73a874a1d
Saikat Dutta <saikatdutta1991@gmail.com>
2020-04-12 20:41:37 +0530

    zigzag string print

--- zigzag_string.js
+++ zigzag_string.js
@@ -1,3 +1,17 @@
+/**
+ * This problem was asked by PayPal.
+
+Given a string and a number of lines k, print the string in zigzag form. In zigzag, characters are printed out diagonally from top left to bottom right until reaching the kth line, then back up to top right, and so on.
+
+For example, given the sentence "thisisazigzag" and k = 4, you should print:
+
+t     a     g
+ h   s z   a
+  i i   i z
+   s     g
+
+ */
+
 class Zigzag {
   /**
    * Constructor
@@ -73,3 +87,13 @@ class Zigzag {
 
 const z = new Zigzag("iamsaikatdutta", 4, "-");
 z.execute().print();
+
+
+/**
+ * Output:
+ * 
+ *  i-----k-----t-
+    -a---i-a---t-a
+    --m-a---t-u---
+    ---s-----d----
+ */
